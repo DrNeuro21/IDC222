7ff842ab2e60a70c77bfd1a751836a749b9a7d23
Meetanshu Deorao Kumbhare <meetanshu21@gmail.com>
2026-01-25 09:08:55 +0530

    Add files via upload

---
+++ addtwonumberwithtemplates.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+	
+	float a = 10.10;
+	float b = 20.20;
+	
+	cout << add<int>(x,y) <<endl;
+	cout << add<float>(a,b) << endl;
+	
+	return 0;
+}		
